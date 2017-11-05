3a4a37e3d4d2e6b36164a7b0ba08541dcc18a280
weichih25 <weichih25@gmail.com>
2017-11-05 16:02:31 +0800

    init

---
+++ code/SignalSmoother/Assets/Arduino.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+using System;
+using Assets;
+
+namespace Assets
+{
+    public class Arduino : MonoBehaviour
+    {
+
+        // Use this for initialization
+
+        public string comportName;
+        public int baudRate;
+        public StateMachine stateMachine;
+        private SerialPort port;
+
+
+        void Start()
+        {
+
+            port = new SerialPort(comportName, baudRate);
+           // port.Open();
+           // port.ReadTimeout = 1;
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+
+            port.Open();
+            port.ReadTimeout = 1;
+            if (port.IsOpen)
+            {
+                try
+                {
+                    string s = port.ReadLine();
+                    
+                   // Debug.Log(s);
+                    this.stateMachine.SerialPortGetMsg(s);
+               
+
+                }
+                catch
+                {
+
+                }
+            }
+
+            port.DiscardOutBuffer();
+            port.DiscardInBuffer();
+            port.Close();
+        }
+
+    }
+}
